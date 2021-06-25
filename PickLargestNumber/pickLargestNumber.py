716ed2a5c79232dba8475bf0aaba148c6d8923e5
Aditya Prasad <aprasad4@usc.edu>
2021-06-25 06:21:00 -0400

    Little update to PickLargestNumber

--- PickLargestNumber/pickLargestNumber.py
+++ PickLargestNumber/pickLargestNumber.py
@@ -3,19 +3,21 @@ import random
 from tqdm import tqdm
 import sys
 
-def oneRun():
+def oneRun(debug = False):
     envelope1 = np.random.normal(110, 1)
     envelope2 = np.random.normal(110, 1)
     t = np.random.normal(0, 1)
-    #python shenanigan
+    # Pick one of the two envelopes
     randomchoice = random.choice((envelope1, envelope2))
-    #print(f"Picked: {randomchoice}, t-value: {t}")
+    if debug:
+        print(f"Picked: {randomchoice}, t-value: {t}")
     if (randomchoice < t):
         if (randomchoice == envelope1):
             randomchoice = envelope2
         else:
             randomchoice = envelope1
-    #print(f"Chose {randomchoice}, truevalue:{max(envelope1, envelope2)}")
+    if debug:
+        print(f"Chose {randomchoice}, truevalue:{max(envelope1, envelope2)}")
     return randomchoice == max(envelope1, envelope2)
 
 def multipleRuns(total):
@@ -25,9 +27,11 @@ def multipleRuns(total):
             correct += 1
     return correct / total
 
-if len(sys.argv) < 2:
-    numruns = 1000000
-else:
-    numruns = int(sys.argv[1])
-percent = multipleRuns(numruns)
-print(f"We got {percent*100}%")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        numruns = 1000000
+    else:
+        numruns = int(sys.argv[1])
+
+    percent = multipleRuns(numruns) * 100
+    print("Split method: %.2f%%" % (percent))
